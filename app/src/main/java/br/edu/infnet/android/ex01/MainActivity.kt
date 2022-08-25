4fa8f48ebc84622da3a4828162f716aa7f1b34ee
Paulo Marinho <tommymolto@gmail.com>
2022-08-25 16:07:43 -0300

    v2

--- app/src/main/java/br/edu/infnet/android/ex01/MainActivity.kt
+++ app/src/main/java/br/edu/infnet/android/ex01/MainActivity.kt
@@ -6,12 +6,14 @@ import android.util.Log
 import android.view.View
 import android.widget.Toast
 import br.edu.infnet.android.ex01.databinding.ActivityMainBinding
+import br.edu.infnet.android.ex01.model.Aluno
 import com.google.android.material.snackbar.Snackbar
 
 
 class MainActivity : AppCompatActivity() {
     val TAG = "QUEM VOTEI"
-
+    val botao = "CADASTRAR"
+    val alunos = mutableListOf<Aluno>()
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -19,15 +21,30 @@ class MainActivity : AppCompatActivity() {
         binding =  ActivityMainBinding.inflate(layoutInflater)
 
         setContentView(binding.root)
-
+        binding.textNome.setText("Digite seu Nome")
+        binding.textNome.setOnFocusChangeListener { _, hasFocus ->
+            if (hasFocus)
+                binding.textNome.setText("")
+        }
         Log.d("INFO", "Cliquei")
 
-
+        binding.btnSalvaUsuario.setOnClickListener {view ->
+            cliqueiAqui(view)
+        }
 
 
 
     }
-
+    fun cliqueiAqui( view: View){
+        binding.btnSalvaUsuario.setText("Salvo")
+        alunos.add(Aluno(
+            binding.textNome.text.toString(),
+            binding.notaUm.text.toString().toDouble(),
+            binding.notaDois.text.toString().toDouble()
+        ))
+        Toast.makeText(view.context, "You clicked me.", Toast.LENGTH_SHORT).show()
+        Log.d("TOTAL", "TOTAL DE ALUNOS : ${alunos.size}")
+    }
     override fun onStart() {
         super.onStart()
         Log.d(TAG , "Entrou onStart")
