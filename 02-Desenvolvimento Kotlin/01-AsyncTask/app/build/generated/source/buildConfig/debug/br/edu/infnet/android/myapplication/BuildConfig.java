9551afca0bb23e8d8d256113f00d0442c003344c
Paulo Marinho <tommymolto@gmail.com>
2022-10-17 15:59:30 -0300

    Room

---
+++ 02-Desenvolvimento Kotlin/01-AsyncTask/app/build/generated/source/buildConfig/debug/br/edu/infnet/android/myapplication/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package br.edu.infnet.android.myapplication;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "br.edu.infnet.android.myapplication";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
