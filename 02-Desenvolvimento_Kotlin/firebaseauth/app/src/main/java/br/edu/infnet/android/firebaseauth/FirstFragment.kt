24242f96d5ebafbbe77d9611a6a6c23bf1403893
Paulo Marinho <tommymolto@gmail.com>
2022-11-20 21:33:13 -0300

    indo pro fb

--- 02-Desenvolvimento_Kotlin/firebaseauth/app/src/main/java/br/edu/infnet/android/firebaseauth/FirstFragment.kt
+++ 02-Desenvolvimento_Kotlin/firebaseauth/app/src/main/java/br/edu/infnet/android/firebaseauth/FirstFragment.kt
@@ -1,5 +1,6 @@
 package br.edu.infnet.android.firebaseauth
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,11 +8,13 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import br.edu.infnet.android.firebaseauth.databinding.FragmentFirstBinding
+import com.facebook.AccessToken
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginResult
 import com.facebook.login.widget.LoginButton
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.ktx.auth
 import com.google.firebase.ktx.Firebase
@@ -57,13 +60,32 @@ class FirstFragment : Fragment() {
             }
 
                 override fun onSuccess(result: LoginResult?) {
+                    handleFacebookAccessToken(result?.accessToken!!)
                     Log.d("INFO", "RESULTADO=${result?.accessToken?.token}")
                 }
             })
         return binding.root
 
     }
+    private fun handleFacebookAccessToken(token: AccessToken) {
+        Log.d(TAG, "handleFacebookAccessToken:$token")
 
+        val credential = FacebookAuthProvider.getCredential(token.token)
+        auth.signInWithCredential(credential)
+            .addOnCompleteListener(requireActivity()) { task ->
+                if (task.isSuccessful) {
+                    // Sign in success, update UI with the signed-in user's information
+                    Log.d(TAG, "signInWithCredential:success")
+                    val user = auth.currentUser
+                    //updateUI(user)
+                } else {
+                    // If sign in fails, display a message to the user.
+                    Log.w(TAG, "signInWithCredential:failure", task.exception)
+
+                    //updateUI(null)
+                }
+            }
+    }
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
